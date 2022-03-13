48c1f43bd11a0bf202e5c76ca425f999b8e43e7b
Mike Abbey <mihaiavram72@gmail.com>
2022-03-14 00:43:15 +0100

    Initial Commit

---
+++ Domain/Interfaces/IToDoHome.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Models;
+
+public interface IToDoHome
+{
+    public Task<ICollection<Todo>> GetAsync();
+    public Task<Todo> GetById(int id);
+    public Task<Todo> AddAsync(Todo todo);
+    public Task DeleteAsync(int id);
+    public Task UpdateAsync(Todo todo);
+}
---
+++ FileData/DataAccess/ToDoFileDAO.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Models;
+
+namespace FileData.DataAccess;
+
+public class ToDoFileDAO : IToDoHome
+{
+    private FileContext fileContext;
+
+    public ToDoFileDAO(FileContext fileContext)
+    {
+        this.fileContext = fileContext;
+    }
+    public Task<ICollection<Todo>> GetAsync()
+    {
+        ICollection<Todo> todos = fileContext.Todos;
+        return  Task.FromResult(todos);
+    }
+
+    public Task<Todo> GetById(int id)
+    {
+        return Task.FromResult(fileContext.Todos.First(t => t.Id == id));    }
+    
+    public async Task<Todo> AddAsync(Todo todo)
+    {
+        int largestId = fileContext.Todos.Max(t => t.Id);
+        int nextId = largestId + 1;
+        todo.Id = nextId;
+        fileContext.Todos.Add(todo);
+        fileContext.SaveChanges();
+        return todo;
+    }
+
+
+    public async Task DeleteAsync(int id)
+    {
+        Todo t = fileContext.Todos.First(t => t.Id == id);
+        fileContext.Todos.Remove(t);
+        fileContext.SaveChanges();
+       
+    }
+
+    public async Task UpdateAsync(Todo todo)
+    {
+        Todo t = fileContext.Todos.First(t => t.Id == todo.Id);
+        t.OwnerId = todo.OwnerId;
+        t.IsCompleted = todo.IsCompleted;
+        fileContext.SaveChanges();
+
+    }
+}
